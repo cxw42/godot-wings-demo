98146b6110c58a76763a3cd886f2c1132b024eb1
Chris White <cxwembedded@gmail.com>
2026-02-21 12:10:28 -0500

    Reformat to 4-space hard tabs

--- top_node_simpler.gd
+++ top_node_simpler.gd
@@ -12,78 +12,87 @@ var b: float
 
 var need_rebuild := false
 
+
 func _ready() -> void:
-    prints("ready", ((a_beam.get_child(1) as CollisionShape3D).shape as BoxShape3D).size.y)
-    a_length.value = ((a_beam.get_child(1) as CollisionShape3D).shape as BoxShape3D).size.y
-    b_length.value = ((b_beam.get_child(1) as CollisionShape3D).shape as BoxShape3D).size.y
-    a_length.enabled = true
-    b_length.enabled = true
+	prints("ready", ((a_beam.get_child(1) as CollisionShape3D).shape as BoxShape3D).size.y)
+	a_length.value = ((a_beam.get_child(1) as CollisionShape3D).shape as BoxShape3D).size.y
+	b_length.value = ((b_beam.get_child(1) as CollisionShape3D).shape as BoxShape3D).size.y
+	a_length.enabled = true
+	b_length.enabled = true
 
 
 #func _process(_delta: float) -> void:
 #    prints("process", ((a_beam.get_child(1) as CollisionShape3D).shape as BoxShape3D).size.y)
 
+
 ## Resize a beam having a CSGMesh3D child and a CollisionShape3D child,
 ## in that order.
 func resize_beam_(beam: Node3D, length_m: float):
-    if not beam:
-        return
+	if not beam:
+		return
 
-    var mesh := beam.get_child(0) as CSGMesh3D
-    var box := mesh.mesh as BoxMesh
-    box.size.y = length_m
+	var mesh := beam.get_child(0) as CSGMesh3D
+	var box := mesh.mesh as BoxMesh
+	box.size.y = length_m
 
-    var collision := beam.get_child(1) as CollisionShape3D
-    var shape := collision.shape as BoxShape3D
-    shape.size.y = length_m
+	var collision := beam.get_child(1) as CollisionShape3D
+	var shape := collision.shape as BoxShape3D
+	shape.size.y = length_m
+
 
 ## inches to meters.  The GUI is in inches.
 func i2m(inches: float):
-    return inches/39.37
+	return inches / 39.37
+
 
 func rebuild_():
-    var a_m = i2m(a)
-    var b_m = i2m(b)
+	var a_m = i2m(a)
+	var b_m = i2m(b)
 
-    # Resize
-    resize_beam_(a_beam, a_m)
-    resize_beam_(b_beam, b_m)
+	# Resize
+	resize_beam_(a_beam, a_m)
+	resize_beam_(b_beam, b_m)
 
-    if false: # XXX experiment
-        # Reposition the beams and end-of-beam joints, starting from the base (A).
-        # Each beam's Y axis extends along the beam, away from A.
-        var a_bottom = a_beam.global_position - a_beam.global_basis.y.normalized() * Vector3(0,a_m/2,0)
-        #a_to_b.global_position = a_bottom
-        b_beam.global_position = a_bottom + b_beam.global_basis.y.normalized() * Vector3(0, b_m/2, 0)
+	if false:  # XXX experiment
+		# Reposition the beams and end-of-beam joints, starting from the base (A).
+		# Each beam's Y axis extends along the beam, away from A.
+		var a_bottom = (
+			a_beam.global_position - a_beam.global_basis.y.normalized() * Vector3(0, a_m / 2, 0)
+		)
+		#a_to_b.global_position = a_bottom
+		b_beam.global_position = (
+			a_bottom + b_beam.global_basis.y.normalized() * Vector3(0, b_m / 2, 0)
+		)
 
-        #var a_top = a_beam.global_position + a_beam.global_basis.y.normalized() * Vector3(0,a_m/2,0)
+		#var a_top = a_beam.global_position + a_beam.global_basis.y.normalized() * Vector3(0,a_m/2,0)
 
 
 func _unhandled_input(event: InputEvent) -> void:
-    if event.is_action_pressed("ui_cancel"):
-        get_tree().quit()
+	if event.is_action_pressed("ui_cancel"):
+		get_tree().quit()
+
 
 func _on_desired_angle_value_changed(value: float) -> void:
-    b_beam.desired_angle = value
+	b_beam.desired_angle = value
 
 
 func _on_a_length_value_changed(value: float) -> void:
-    prints("a length changed", value)
-    a = value
-    need_rebuild = true
+	prints("a length changed", value)
+	a = value
+	need_rebuild = true
 
 
 func _on_b_length_value_changed(value: float) -> void:
-    prints("b length changed", value)
-    b = value
-    need_rebuild = true
-
+	prints("b length changed", value)
+	b = value
+	need_rebuild = true
 
 
 func _on_check_box_toggled(toggled_on: bool) -> void:
-    b_beam.enabled = toggled_on
+	b_beam.enabled = toggled_on
+
 
 func _physics_process(_delta: float) -> void:
-    if need_rebuild:
-        rebuild_()
-        need_rebuild = false
+	if need_rebuild:
+		rebuild_()
+		need_rebuild = false
