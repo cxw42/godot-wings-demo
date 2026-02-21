bfe55bf2f08ba23f75b6b5e53ea7599e0bab4d1a
Chris White <cxwembedded@gmail.com>
2026-02-21 12:08:27 -0500

    pull script out of tscn

--- h_slider_label.gd
+++ h_slider_label.gd
@@ -8,20 +8,20 @@ extends HBoxContainer
 @export_range(-1000, 1000) var maximum
 
 @export var value: float:
-	set(p_new):
-		value = p_new
-		notify_property_list_changed()
-		if not slider:
-			return
-		slider.value = p_new
+    set(p_new):
+        value = p_new
+        notify_property_list_changed()
+        if not slider:
+            return
+        slider.value = p_new
 
 @export var enabled: bool = true:
-	set(p_new):
-		enabled = p_new
-		notify_property_list_changed()
-		if not slider:
-			return
-		slider.editable = enabled
+    set(p_new):
+        enabled = p_new
+        notify_property_list_changed()
+        if not slider:
+            return
+        slider.editable = enabled
 
 @export var format_string: String = "%.f"
 
@@ -29,16 +29,16 @@ signal value_changed(value: float)
 
 
 func _ready():
-	label.text = name
-	slider.min_value = minimum
-	slider.max_value = maximum
-	slider.step = (maximum - minimum) / 100.0
-	self.enabled = enabled
-	self.value = value
+    label.text = name
+    slider.min_value = minimum
+    slider.max_value = maximum
+    slider.step = (maximum - minimum) / 100.0
+    self.enabled = enabled
+    self.value = value
 
 
 func _on_slider_value_changed(new_value: float) -> void:
-	if not enabled:
-		return
-	value_changed.emit(new_value)
-	value_readout.text = format_string % new_value
+    if not enabled:
+        return
+    value_changed.emit(new_value)
+    value_readout.text = format_string % new_value
